5011c0c677ce3276d1580ac4c69e95989538a10f
marchbold <marchbold@users.noreply.github.com>
2022-06-09 13:16:45 +1000

    feat(scripts): add multi-instance flag to avoid multiple instances of apm interrupting each other (resolves #142)

--- client/src/apm.bat
+++ client/src/apm.bat
@@ -8,5 +8,5 @@ for /f "tokens=*" %%a in ('where adl.exe') do set ADL_DIR=%%a
 for %%a in (%ADL_DIR%) do set AIR_BIN_DIR=%%~dpa
 for %%a in (%AIR_BIN_DIR:~0,-1%) do set AIR_DIR=%%~dpa
 
-adl -profile extendedDesktop -nodebug -cmd "%~dp0\apm.xml" -- -workingdir "%cd%" -airdir "%AIR_DIR% " -uname "windows" %*
+adl -profile extendedDesktop -nodebug -multi-instance -cmd "%~dp0\apm.xml" -- -workingdir "%cd%" -airdir "%AIR_DIR% " -uname "windows" %*
 
